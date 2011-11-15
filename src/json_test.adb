411b8080886d91bc3c9b5520742a9a7c72637366
Thomas Løcke <thomas@12boo.net>
2011-11-15 21:36:10 +0100

    Minor changes. Added some output delimiters.

--- src/json_test.adb
+++ src/json_test.adb
@@ -10,6 +10,7 @@ procedure Json_Test is
    Company_JSON : JSON_Value := Create_Object;
    Test_JSON    : JSON_Value;
 begin
+   --  Build the company JSON
    Set_Core_Company_Data (Obj     => Company_JSON,
                           Name    => "AdaHeads K/S",
                           VAT_No  => "134679-zoink",
@@ -34,20 +35,41 @@ begin
                         Comment     => "Branch office, Thomas Løcke");
 
    Add_Person (Obj   => Company_JSON,
-               Name  => "Thomas Løcke",
+               Name  => "Jacob Sparre Andersen",
                Title => "Codemonkey",
-               Email => "thomas.locke@adaheads.com");
+               Email => "jacob.sparre.andersen@adaheads.com");
 
    Add_Person (Obj   => Company_JSON,
                Name  => "Kim Rostgaard Christensen",
                Title => "Codemonkey",
                Email => "kim.rostgaard.christensen@adaheads.com");
 
-   Put_Line (Write (Company_JSON, False));
+   Add_Person (Obj   => Company_JSON,
+               Name  => "Thomas Løcke",
+               Title => "Codemonkey",
+               Email => "thomas.locke@adaheads.com");
 
+   Add_Person (Obj   => Company_JSON,
+               Name  => "Ulrik Hørlyk Hjort",
+               Title => "Codemonkey",
+               Email => "ulrik.horlyk.hjort@adaheads.com");
+
+   --  Output the company JSON.
+   New_Line;
+   Put_Line ("--> Company_JSON Start <--");
+   Put_Line (Write (Company_JSON, False));
+   Put_Line ("--> Company_JSON End <--");
+   New_Line;
+
+   --  Load the test.json JSON object into the Test_JSON object.
    Test_JSON := Read (Strm     => Load_File ("test.json"),
                       Filename => "");
 
+   --  Iterate the Test_JSON object. The Handler procedure is responsible for
+   --  outputting the contents.
+   Put_Line ("--> Test_JSON Start <--");
    Map_JSON_Object (Val   => Test_JSON,
                     CB    => Handler'Access);
+   Put_Line ("--> Test_JSON End <--");
+   New_Line;
 end Json_Test;
