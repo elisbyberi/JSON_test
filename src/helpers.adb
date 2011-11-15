cee2acea4a9fdeafdf00efeb910b6c86073335f5
Thomas Løcke <thomas@12boo.net>
2011-11-15 21:53:28 +0100

    Added license header to relevant files.

--- src/helpers.adb
+++ src/helpers.adb
@@ -1,3 +1,24 @@
+-------------------------------------------------------------------------------
+--                                                                           --
+--                                Helpers                                    --
+--                                                                           --
+--                                 BODY                                      --
+--                                                                           --
+--                     Copyright (C) 2011, Thomas Løcke                      --
+--                                                                           --
+--  Helpers is free software;  you can  redistribute it  and/or modify       --
+--  it under terms of the  GNU General Public License as published  by the   --
+--  Free Software  Foundation;  either version 2,  or (at your option) any   --
+--  later version.  Helpers is distributed in the hope that it will be       --
+--  useful, but WITHOUT ANY WARRANTY;  without even the  implied warranty of --
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU        --
+--  General Public License for  more details.  You should have  received a  --
+--  copy of the GNU General Public License  distributed with JSON_Test.      --
+--  If not, write  to  the  Free Software Foundation,  51  Franklin  Street, --
+--  Fifth  Floor, Boston, MA 02110 - 1301, USA.                              --
+--                                                                           --
+-------------------------------------------------------------------------------
+
 with Ada.Direct_IO;
 with Ada.Directories;
 with Ada.Text_IO;
--- src/json_test.adb
+++ src/json_test.adb
@@ -1,3 +1,22 @@
+-------------------------------------------------------------------------------
+--                                                                           --
+--                                JSON_Test                                  --
+--                                                                           --
+--                     Copyright (C) 2011, Thomas Løcke                      --
+--                                                                           --
+--  JSON_Test is free software;  you can  redistribute it  and/or modify     --
+--  it under terms of the  GNU General Public License as published  by the   --
+--  Free Software  Foundation;  either version 2,  or (at your option) any   --
+--  later version.  JSON_Test is distributed in the hope that it will be     --
+--  useful, but WITHOUT ANY WARRANTY;  without even the  implied warranty of --
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU        --
+--  General Public License for  more details.  You should have  received  a  --
+--  copy of the GNU General Public License  distributed with JSON_Test.      --
+--  If not, write  to  the  Free Software Foundation,  51  Franklin  Street, --
+--  Fifth  Floor, Boston, MA 02110 - 1301, USA.                              --
+--                                                                           --
+-------------------------------------------------------------------------------
+
 with Ada.Text_IO;
 with GNATCOLL.JSON;
 with Helpers;
